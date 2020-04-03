52e594dac8cc4090cd0abfbe4c9957cbcf5ae688
Diego Capassi <diego.capassi.moreira@gmail.com>
2020-04-03 00:30:07 -0300

    Styled the Cart page

--- src/Pages/Cart/index.js
+++ src/Pages/Cart/index.js
@@ -1,5 +1,65 @@
-import React from "react"
+import React from "react";
+import { Container, ProductTable, Total } from "./styles.js";
+import {
+  MdDelete,
+  MdAddCircleOutline,
+  MdRemoveCircleOutline
+} from "react-icons/md";
 
-export default function Cart(){
-    return <h1>Carrinho</h1>
+export default function Cart() {
+  return (
+    <Container>
+      <ProductTable>
+        <thead>
+          <tr>
+            <th />
+            <th>PRODUTO</th>
+            <th>QTD</th>
+            <th>SUBTOTAL</th>
+            <th />
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <img
+                src="https://static.netshoes.com.br/produtos/tenis-nike-downshifter-9-masculino/26/HZM-1276-026/HZM-1276-026_zoom2.jpg?ts=1585801973&ims=326x"
+                alt="tenis"
+              />
+            </td>
+            <td>
+              <strong>Tênis muito massa!</strong>
+              <span>R129,90</span>
+            </td>
+            <td>
+              <div>
+                <button type="button">
+                  <MdRemoveCircleOutline size={20} color="#7159c1" />
+                </button>
+                <input type="number" readOnly value={2} />
+                <button type="button">
+                  <MdAddCircleOutline size={20} color="#7159c1" />
+                </button>
+              </div>
+            </td>
+            <td>
+              <strong>R$ 259,80</strong>
+            </td>
+            <td>
+              <button type="button">
+                <MdDelete size={20} color="#7159c1" />
+              </button>
+            </td>
+          </tr>
+        </tbody>
+      </ProductTable>
+      <footer>
+        <button type="button"> Finalizar pedido</button>
+        <Total>
+          <span>TOTAL</span>
+          <strong>R$1920,28</strong>
+        </Total>
+      </footer>
+    </Container>
+  );
 }
